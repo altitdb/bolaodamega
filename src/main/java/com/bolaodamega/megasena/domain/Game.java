bdc8b94035054b618320748b425410ad42876660
RicardoLuizSilveira <ricardo.silveira@gmail.com>
2016-05-03 23:44:46 -0300

    Implementado o método public ArrayList<Integer> getNumbers();

--- src/main/java/com/bolaodamega/megasena/domain/Game.java
+++ src/main/java/com/bolaodamega/megasena/domain/Game.java
@@ -1,60 +1,74 @@
 package com.bolaodamega.megasena.domain;
 
+import java.util.ArrayList;
+
 public class Game {
 
-    private Integer number01;
-    private Integer number02;
-    private Integer number03;
-    private Integer number04;
-    private Integer number05;
-    private Integer number06;
+	private Integer number01;
+	private Integer number02;
+	private Integer number03;
+	private Integer number04;
+	private Integer number05;
+	private Integer number06;
 
-    public Integer getNumber01() {
-        return number01;
-    }
+	public Integer getNumber01() {
+		return number01;
+	}
 
-    public void setNumber01(Integer number01) {
-        this.number01 = number01;
-    }
+	public void setNumber01(Integer number01) {
+		this.number01 = number01;
+	}
 
-    public Integer getNumber02() {
-        return number02;
-    }
+	public Integer getNumber02() {
+		return number02;
+	}
 
-    public void setNumber02(Integer number02) {
-        this.number02 = number02;
-    }
+	public void setNumber02(Integer number02) {
+		this.number02 = number02;
+	}
 
-    public Integer getNumber03() {
-        return number03;
-    }
+	public Integer getNumber03() {
+		return number03;
+	}
 
-    public void setNumber03(Integer number03) {
-        this.number03 = number03;
-    }
+	public void setNumber03(Integer number03) {
+		this.number03 = number03;
+	}
 
-    public Integer getNumber04() {
-        return number04;
-    }
+	public Integer getNumber04() {
+		return number04;
+	}
 
-    public void setNumber04(Integer number04) {
-        this.number04 = number04;
-    }
+	public void setNumber04(Integer number04) {
+		this.number04 = number04;
+	}
 
-    public Integer getNumber05() {
-        return number05;
-    }
+	public Integer getNumber05() {
+		return number05;
+	}
 
-    public void setNumber05(Integer number05) {
-        this.number05 = number05;
-    }
+	public void setNumber05(Integer number05) {
+		this.number05 = number05;
+	}
 
-    public Integer getNumber06() {
-        return number06;
-    }
+	public Integer getNumber06() {
+		return number06;
+	}
 
-    public void setNumber06(Integer number06) {
-        this.number06 = number06;
-    }
+	public void setNumber06(Integer number06) {
+		this.number06 = number06;
+	}
+
+	public ArrayList<Integer> getNumbers() {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(getNumber01());
+		numbers.add(getNumber02());
+		numbers.add(getNumber03());
+		numbers.add(getNumber04());
+		numbers.add(getNumber05());
+		numbers.add(getNumber06());
+
+		return numbers;
+	}
 
 }
