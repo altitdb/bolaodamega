3bd5059ac7d5e661faaea12ea4a0dd424ef648ef
Altieres de Matos <altieres.matos@matera.com>
2016-05-17 21:27:26 -0300

    ADD ROLE TEST NUMBERS SAME ROW

---
+++ src/test/java/com/bolaodamega/megasena/roles/NumbersSameRowTest.java
@@ -0,0 +1,168 @@
+package com.bolaodamega.megasena.roles;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.bolaodamega.megasena.domain.Game;
+
+public class NumbersSameRowTest {
+
+    private Role role = new NumbersSameRow();
+    
+    @Test
+    public void shouldShowInvalidSameFirstRow() {
+        Game game = new Game();
+        game.setNumber01(1);
+        game.setNumber02(2);
+        game.setNumber03(3);
+        game.setNumber04(4);
+        game.setNumber05(5);
+        game.setNumber06(6);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidSameSecondRow() {
+        Game game = new Game();
+        game.setNumber01(13);
+        game.setNumber02(14);
+        game.setNumber03(15);
+        game.setNumber04(17);
+        game.setNumber05(18);
+        game.setNumber06(20);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidSameThirdRow() {
+        Game game = new Game();
+        game.setNumber01(23);
+        game.setNumber02(25);
+        game.setNumber03(27);
+        game.setNumber04(28);
+        game.setNumber05(29);
+        game.setNumber06(30);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidSameFourthRow() {
+        Game game = new Game();
+        game.setNumber01(31);
+        game.setNumber02(32);
+        game.setNumber03(35);
+        game.setNumber04(37);
+        game.setNumber05(39);
+        game.setNumber06(40);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidSameFifthRow() {
+        Game game = new Game();
+        game.setNumber01(41);
+        game.setNumber02(42);
+        game.setNumber03(45);
+        game.setNumber04(47);
+        game.setNumber05(49);
+        game.setNumber06(50);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidSameSixthRow() {
+        Game game = new Game();
+        game.setNumber01(52);
+        game.setNumber02(53);
+        game.setNumber03(54);
+        game.setNumber04(55);
+        game.setNumber05(59);
+        game.setNumber06(60);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidWithFourNumberAtTheBegin() {
+        Game game = new Game();
+        game.setNumber01(41);
+        game.setNumber02(42);
+        game.setNumber03(45);
+        game.setNumber04(47);
+        game.setNumber05(53);
+        game.setNumber06(55);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidWithFourNumberAtTheMiddle() {
+        Game game = new Game();
+        game.setNumber01(31);
+        game.setNumber02(42);
+        game.setNumber03(45);
+        game.setNumber04(47);
+        game.setNumber05(48);
+        game.setNumber06(60);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidWithFourNumberAtTheEnd() {
+        Game game = new Game();
+        game.setNumber01(31);
+        game.setNumber02(32);
+        game.setNumber03(45);
+        game.setNumber04(47);
+        game.setNumber05(48);
+        game.setNumber06(50);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidWithFiveNumberAtTheBegin() {
+        Game game = new Game();
+        game.setNumber01(41);
+        game.setNumber02(42);
+        game.setNumber03(45);
+        game.setNumber04(47);
+        game.setNumber05(48);
+        game.setNumber06(55);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowInvalidWithFiveNumberAtTheEnd() {
+        Game game = new Game();
+        game.setNumber01(31);
+        game.setNumber02(42);
+        game.setNumber03(45);
+        game.setNumber04(47);
+        game.setNumber05(48);
+        game.setNumber06(50);
+        boolean result = role.validate(game);
+        assertTrue(result);
+    }
+    
+    @Test
+    public void shouldShowValid() {
+        Game game = new Game();
+        game.setNumber01(1);
+        game.setNumber02(12);
+        game.setNumber03(25);
+        game.setNumber04(37);
+        game.setNumber05(48);
+        game.setNumber06(52);
+        boolean result = role.validate(game);
+        assertFalse(result);
+    }
+}
