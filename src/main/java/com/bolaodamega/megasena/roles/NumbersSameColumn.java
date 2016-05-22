fbc1586c8315e731c98c4f8e07d97dbf7ba1ca83
RicardoLuizSilveira <ricardo.silveira@gmail.com>
2016-05-22 13:49:11 -0300

    Implementation of same column role

--- src/main/java/com/bolaodamega/megasena/roles/NumbersSameColumn.java
+++ src/main/java/com/bolaodamega/megasena/roles/NumbersSameColumn.java
@@ -1,11 +1,22 @@
 package com.bolaodamega.megasena.roles;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.bolaodamega.megasena.domain.Game;
 
 public class NumbersSameColumn implements Role {
 
     @Override
     public boolean validate(Game game) {
+    	List<Integer> numbers = game.getNumbers();
+    	int columns[] = new int[10];
+    	Arrays.fill(columns, 0);
+    	for(Integer number : numbers){
+    		int column = (number + 10)%10;
+    		columns[column]++;
+    		if(columns[column] > 3) return true;
+    	}
         return false;
     }
 
