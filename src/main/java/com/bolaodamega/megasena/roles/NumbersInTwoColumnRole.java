8d49698dffb705a8bb6c47957bbd6d8dbf3869bb
RicardoLuizSilveira <ricardo.silveira@gmail.com>
2016-06-21 16:25:21 -0300

    ROLE NUMBERS IN TWO COLUMNS IMPLEMENTATION

--- src/main/java/com/bolaodamega/megasena/roles/NumbersInTwoColumnRole.java
+++ src/main/java/com/bolaodamega/megasena/roles/NumbersInTwoColumnRole.java
@@ -1,11 +1,28 @@
 package com.bolaodamega.megasena.roles;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.bolaodamega.megasena.domain.Game;
 
 public class NumbersInTwoColumnRole implements Role {
 
     @Override
     public boolean validate(Game game) {
+    	List<Integer> numbers = game.getNumbers();
+    	int columns[] = new int[10];
+    	Arrays.fill(columns, 0);	
+    	int columnCount = 0;
+    	for(Integer number : numbers){
+    		int column = (number + 10)%10;
+    		if(columns[column] == 0) {
+    			columns[column]++;
+    			columnCount++;
+    		}
+    	}
+    	if(columnCount < 3){
+    		return true;
+    	}
         return false;
     }
 
