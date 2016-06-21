fc03de661ec29abcf7e275bfe1469d2932346abe
RicardoLuizSilveira <ricardo.silveira@gmail.com>
2016-06-21 16:24:22 -0300

    SEQUENCES ROLE IMPLEMENTATION AND TESTS

--- src/main/java/com/bolaodamega/megasena/roles/NumberGroupSequentialRole.java
+++ src/main/java/com/bolaodamega/megasena/roles/NumberGroupSequentialRole.java
@@ -1,11 +1,34 @@
 package com.bolaodamega.megasena.roles;
 
+import java.util.List;
+
 import com.bolaodamega.megasena.domain.Game;
 
 public class NumberGroupSequentialRole implements Role {
 
 	@Override
 	public boolean validate(Game game) {
+		List<Integer> numbers = game.getNumbers();
+		int sequentialCount = 0;
+		for(int i = 1; i < numbers.size();){
+			int actual = numbers.get(i);
+			int before = numbers.get(i -1);
+			if(actual == (before + 1)){
+				sequentialCount++;
+//				i++;
+				while(i < numbers.size() && actual == (before +1)){
+					actual = numbers.get(i);
+					before = numbers.get(i -1);
+					i++;
+				}
+			}else{
+				i++;
+			}
+			if(sequentialCount > 1){
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
--- src/test/java/com/bolaodamega/megasena/roles/NumbersGroupSequentialRoleTest.java
+++ src/test/java/com/bolaodamega/megasena/roles/NumbersGroupSequentialRoleTest.java
@@ -12,7 +12,8 @@ import com.bolaodamega.megasena.domain.Game;
  * Example 02 (Incorrect): 1, 2, 3, 11, 12, 13
  * Example 03 (Incorrect): 30, 31, 32, 45, 46, 47
  * Example 03 (Incorrect): 7, 8, 35, 36, 46, 47
- * Example 04 (Correct): 1, 2, 3, 8, 11, 20
+ * Example 04 (Incorrect): 1, 2, 8, 9, 32, 47
+ * Example 05 (Correct): 1, 2, 3, 8, 11, 20
  * @author ALM
  *
  */
@@ -47,7 +48,98 @@ public class NumbersGroupSequentialRoleTest {
 	}
 	
 	@Test
-	public void shouldShowInvalidGroupSequenceTwoAndFourNumbers() {
+	public void shouldShowInvalidTwoSequencesBeginAndEnd() {
+		Game game = new Game();
+		game.setNumber01(1);
+		game.setNumber02(2);
+		game.setNumber03(15);
+		game.setNumber04(27);
+		game.setNumber05(42);
+		game.setNumber06(43);
+		boolean isInvalid = role.validate(game);
+		Assert.assertTrue(isInvalid);
+	}
+	
+	@Test
+	public void shouldShowInvalidTwoSequencesMidleAndEnd() {
+		Game game = new Game();
+		game.setNumber01(7);
+		game.setNumber02(14);
+		game.setNumber03(26);
+		game.setNumber04(27);
+		game.setNumber05(42);
+		game.setNumber06(43);
+		boolean isInvalid = role.validate(game);
+		Assert.assertTrue(isInvalid);
+	}
+	
+	@Test
+	public void shouldShowInvalidTwoSequencesBeginAndMidle() {
+		Game game = new Game();
+		game.setNumber01(7);
+		game.setNumber02(8);
+		game.setNumber03(26);
+		game.setNumber04(27);
+		game.setNumber05(42);
+		game.setNumber06(51);
+		boolean isInvalid = role.validate(game);
+		Assert.assertTrue(isInvalid);
+	}
+	
+	@Test
+	public void shouldShowInvalidTwoSequencesMidle() {
+		Game game = new Game();
+		game.setNumber01(7);
+		game.setNumber02(13);
+		game.setNumber03(14);
+		game.setNumber04(27);
+		game.setNumber05(28);
+		game.setNumber06(51);
+		boolean isInvalid = role.validate(game);
+		Assert.assertTrue(isInvalid);
+	}
+	
+	@Test
+	public void shouldShowInvalidTwoSequencesThreeAndTwoBegin() {
+		Game game = new Game();
+		game.setNumber01(7);
+		game.setNumber02(8);
+		game.setNumber03(9);
+		game.setNumber04(15);
+		game.setNumber05(16);
+		game.setNumber06(29);
+		boolean isInvalid = role.validate(game);
+		Assert.assertTrue(isInvalid);
+	}
+	
+	@Test
+	public void shouldShowInvalidTwoSequencesThreeAndTwoEnd() {
+		Game game = new Game();
+		game.setNumber01(7);
+		game.setNumber02(13);
+		game.setNumber03(14);
+		game.setNumber04(15);
+		game.setNumber05(28);
+		game.setNumber06(29);
+		boolean isInvalid = role.validate(game);
+		Assert.assertTrue(isInvalid);
+	}
+	
+	@Test
+	public void shouldShowInvalidTwoSequencesThreeAndTwoWithMiddleGap() {
+		Game game = new Game();
+		game.setNumber01(7);
+		game.setNumber02(8);
+		game.setNumber03(9);
+		game.setNumber04(15);
+		game.setNumber05(28);
+		game.setNumber06(29);
+		boolean isInvalid = role.validate(game);
+		Assert.assertTrue(isInvalid);
+	}
+	
+	@Test
+	public void shouldShowValidGroupSequenceTwoAndFourNumbers() {
 		Game game = new Game();
 		game.setNumber01(1);
 		game.setNumber02(2);
@@ -55,6 +147,32 @@ public class NumbersGroupSequentialRoleTest {
 		game.setNumber04(30);
 		game.setNumber05(41);
 		game.setNumber06(52);
+		boolean isInvalid = role.validate(game);
+		Assert.assertFalse(isInvalid);
+	}
+	
+	@Test
+	public void shouldShowValidGroupSequenceFourAndTwoNumbers() {
+		Game game = new Game();
+		game.setNumber01(1);
+		game.setNumber02(2);
+		game.setNumber03(3);
+		game.setNumber04(4);
+		game.setNumber05(41);
+		game.setNumber06(52);
+		boolean isInvalid = role.validate(game);
+		Assert.assertFalse(isInvalid);
+	}
+	
+	@Test
+	public void shouldShowValidGroupSequenceSixAndNoneNumbers() {
+		Game game = new Game();
+		game.setNumber01(1);
+		game.setNumber02(2);
+		game.setNumber03(3);
+		game.setNumber04(4);
+		game.setNumber05(5);
+		game.setNumber06(6);
 		boolean isInvalid = role.validate(game);
 		Assert.assertFalse(isInvalid);
 	}
