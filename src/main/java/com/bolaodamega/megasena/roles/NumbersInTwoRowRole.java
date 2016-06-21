8d914f01575bc13f83f10e7b73f4b63787ec17d3
RicardoLuizSilveira <ricardo.silveira@gmail.com>
2016-06-21 16:26:08 -0300

    ROLE - NUMBERS IN TWO ROW IMPLEMENTATION

--- src/main/java/com/bolaodamega/megasena/roles/NumbersInTwoRowRole.java
+++ src/main/java/com/bolaodamega/megasena/roles/NumbersInTwoRowRole.java
@@ -1,12 +1,34 @@
 package com.bolaodamega.megasena.roles;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.bolaodamega.megasena.domain.Game;
 
 public class NumbersInTwoRowRole implements Role {
 
     @Override
     public boolean validate(Game game) {
-        return false;
+    	List<Integer> numbers = game.getNumbers();
+    	int[] rows = new int[6];
+    	Arrays.fill(rows, 0);
+    	int rowsCount = 0;
+    	for(Integer number : numbers){
+    		for(int i = 0; i < 6; i++){
+    			int rowLimit = (i + 1) * 10;
+    			if(number <= rowLimit){
+    				if(rows[i] == 0){
+    					rows[i]++;
+    					rowsCount++;
+    				}
+    				if(rowsCount > 2){
+    					return false;
+    				}
+    				break;
+    			}
+    		}
+    	}
+        return true;
     }
 
     @Override
