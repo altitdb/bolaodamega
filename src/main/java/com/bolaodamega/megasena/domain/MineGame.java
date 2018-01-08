81d5afc27da983ab6ac17785a16d903760b63e82
Altieres de Matos <altitdb@gmail.com>
2018-01-08 21:51:25 -0200

    Update MineGame to accept game data/hour

--- src/main/java/com/bolaodamega/megasena/domain/MineGame.java
+++ src/main/java/com/bolaodamega/megasena/domain/MineGame.java
@@ -1,5 +1,8 @@
 package com.bolaodamega.megasena.domain;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -10,6 +13,8 @@ public class MineGame extends Game {
 	@ManyToOne
 	@JoinColumn(name = "USER_ID")
 	private User user;
+	@Column(name = "LAST_UPDATE")
+	private LocalDateTime lastUpdate = LocalDateTime.now();
 
 	public User getUser() {
 		return user;
